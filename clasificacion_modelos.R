ceed701c7ee7556c381bd6bedbcf017eeaeea7e7
iibanez <ignacio.ibanez@usach.cl>
2017-08-21 13:08:29 -0300

    agregado k-means optimizado

--- clasificacion_modelos.R
+++ clasificacion_modelos.R
@@ -2,13 +2,21 @@ library("mclust") #cargar biblioteca
 
 data = read.table("wpbc.data", sep = ",")
 
-colnames(data) = c("id", "c", "t ","r1", "t1", "p1", "a1", "su1", 
-                   "com1", "con1", "pc1", "si1", "df1", 
-                   "r2", "t2", "p2", "a2", "su2", 
+colnames(data) = c("id", "c", "t ","r1", "t1", "p1", "a1", "su1",
+                   "com1", "con1", "pc1", "si1", "df1",
+                   "r2", "t2", "p2", "a2", "su2",
                    "com2", "con2", "pc2", "si2", "df2",
-                   "r3", "t3", "p3", "a3", "su3", 
+                   "r3", "t3", "p3", "a3", "su3",
                    "com3", "con3", "pc3", "si3", "df3",
                    "tt", "eg")
+
+# colnames(data) = c("id", "clase", "tiempo ","radio_1", "textura_1", "perimetro_1", "area_1", "suavidad_1",
+#                   "compacidad_1", "concavidad_1", "puntos_concavos_1", "simetria_1", "dimension_fractal_1",
+#                   "radio_2", "textura_2", "perimetro_2", "area_2", "suavidad_2",
+#                   "compacidad_2", "concavidad_2", "puntos_concavos_2", "simetria_2", "dimension_fractal_2",
+#                   "radio_3", "textura_3", "perimetro_3", "area_3", "suavidad_3",
+#                   "compacidad_3", "concavidad_3", "puntos_concavos_3", "simetria_3", "dimension_fractal_3",
+#                   "tam_tumor", "est_ganglios")
 
 #se eliminan registros con error
 error = data$eg == '?'
@@ -73,3 +81,56 @@ table(class,mod_vev$classification)
 #es graficado un agrupamiento
 plot(mod_eee, what = "classification") 
 legend("bottomright", legend = 1:2, col = mclust.options("classPlotColors"), pch = mclust.options("classPlotSymbols"),title = "Class labels:")
+
+#ver correlación de cada variable segun componentes principales
+data.acp <- prcomp(data[4:35], scale = TRUE)
+
+data.cor <- data.acp$rotation %*% diag(data.acp$sdev)
+
+colors <- c("darkblue", "red", "green")
+barplot(t(data.cor[, 1:3]), beside = TRUE, ylim = c(-1, 1), col = colors, xlab = "Variables", ylab = "Correlación", main = "Correlación CP y variables", las = 2)
+legend("topright", legend = c("CP 1", "CP 2", "CP 3"), fill = colors)
+
+#comparar metodo de agrupamiento por modelos con K-means
+# Las 4 variaciones del algoritmo son: Lloyd, Forgy, MacQueen y Hartigan-Wong. 
+# Para compararlas puede usarse la "distancia intracluster", que es la suma 
+# de las distancia entre los centroide. El algoritmo que tenga la mayor 
+# "distancia intracluster" sería el ganador ya que sería la mejor separación de grupos.
+
+#https://www.youtube.com/watch?v=UwX4Ta78J0U
+#algoritmos utilizados
+algoritmos = c("Hartigan-Wong","Lloyd","Forgy","MacQueen") 
+#cantidad de algorimos
+cantidadAlgoritmos = length(Algoritmos) # guarda la cantidad de algoritmos usados
+#para guardar las iteraciones de cada algotimo que será corrido 50 veces cada uno
+iteraciones = data.frame(intraclase=numeric(),algoritmo=character())
+
+#es corrido cada algorimo 50 veces y almacenado su valor de inercia interclase
+#cada algorimo es corrido dado que se parte con puntos escogidos al azar
+for (i in 1:cantidadAlgoritmos) 
+{
+  for (ii in 1:50) 
+  {
+    modelo      = kmeans(data[4:35],2, algorithm = algoritmos[i]) # 2 corresponde cantidad de grupos
+    iteraciones = rbind(iteraciones,
+                         data.frame(intraclase = modelo$betweenss,
+                                    algoritmo = algoritmos[i]))
+  }
+}
+
+#son sumados los valores obtenidos por cada algorimo
+resultados = tapply(iteraciones$intraclase,iteraciones$algoritmo,mean) 
+#son ordenados los promedios de las inercia entre clasesr
+resultados = sort(resultados,decreasing = T)
+algoritmoGanador = names(resultados[1])
+
+#se ejecuta el algorimo ganador y se le asigna una clase a cada paciente
+kmeansOptimizado = kmeans(data[4:35],2, algorithm = algoritmoGanador)
+
+#comprobar funcionamiento del algorimo kmeans
+table(class,kmeansOptimizado$cluster)
+
+# class   1   2
+#     N  48 100
+#     R  23  23
+# error en 71 casos
